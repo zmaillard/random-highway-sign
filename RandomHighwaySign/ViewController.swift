1caffae781d2658ee298107225db92de3abba122
Zachary Maillard <sagebrushgis@gmail.com>
2015-04-19 16:30:26 -0600

    Added SwiftyJSOn

--- RandomHighwaySign/ViewController.swift
+++ RandomHighwaySign/ViewController.swift
@@ -3,16 +3,23 @@
 //  RandomHighwaySign
 //
 //  Created by Zachary Maillard on 4/19/15.
-//  Copyright (c) 2015 SagebrushGIS. All rights reserved.
-//
 
 import UIKit
+import Alamofire;
+import SwiftyJSON;
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        randomSignRequest();
+        let url = NSURL(string: "http://highwaysign.s3.amazonaws.com/2238726485/2238726485_m.jpg");
+        let data = NSData(contentsOfURL: url!);
+        mainImage.image = UIImage(data:data!);
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func randomSignRequest(){
+        Alamofire.request(.GET, "http://www.sagebrushgis.com/random/?format=json")
+            .responseJSON{(_,_,JSON,_)in
+                    println(JSON["signs"]);
+                
+        }
+    }
 
 }
-
