95d066dd402bcd1eb6b74955fb35ab05e94f8058
Zachary Maillard <sagebrushgis@gmail.com>
2015-05-06 20:27:51 -0600

    Converted to ui scroll view

--- RandomHighwaySign/ViewController.swift
+++ RandomHighwaySign/ViewController.swift
@@ -12,8 +12,9 @@ import SwiftyJSON;
 class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var spinner: UIActivityIndicatorView!
-    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var navItem: UINavigationItem!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
     
     var sign : Sign!;
     
@@ -72,10 +73,78 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         let image = UIImage(data:data!)
         imageView.image = image
+        //imageView.frame = CGRect(origin: CGPoint(x: 0,y: 0), size: image!.size)
+        
+        //scrollView.addSubview(imageView)
+        //scrollView.contentSize = image!.size
+        
+        var doubleTapRecognizer = UITapGestureRecognizer(target:self, action:"scrollViewDoubleTapped:")
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.numberOfTouchesRequired = 1
+        scrollView.addGestureRecognizer(doubleTapRecognizer)
+        
+        // Set the translatesAutoresizingMaskIntoConstraints to NO so that the views autoresizing mask is not translated into auto layout constraints.
+        //imageView.setTranslatesAutoresizingMaskIntoConstraints(false);
+        //scrollView.setTranslatesAutoresizingMaskIntoConstraints(false);
+        
+        // Set up the minimum & maximum zoom scales
+        // configure image and scroll view for scrolling to extents of actual image
+        let widthScale = scrollView.frame.size.width / image!.size.width;
+        let heightScale = scrollView.frame.size.height / image!.size.height;
+        self.scrollView.minimumZoomScale = max(widthScale, heightScale);
+        self.scrollView.maximumZoomScale = 1.0
+        self.scrollView.zoomScale = self.scrollView.minimumZoomScale;
+        self.scrollView.delegate = self
+        centerScrollViewContents()
+        
         spinner.stopAnimating()
         spinner.hidden = true
     }
 
+    func centerScrollViewContents(){
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+        
+        if contentsFrame.size.width < boundsSize.width{
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0
+        }else{
+            contentsFrame.origin.x = 0.0
+        }
+        
+        if contentsFrame.size.height < boundsSize.height{
+            contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0
+        }else{
+            contentsFrame.origin.y = 0.0
+        }
+        
+        imageView.frame = contentsFrame
+    }
+    
+    func scrollViewDoubleTapped(recognizer : UITapGestureRecognizer){
+        let pointInView = recognizer.locationInView(imageView)
+        
+        var newZoomScale = scrollView.zoomScale * 1.5
+        newZoomScale = min (newZoomScale, scrollView.maximumZoomScale)
+        
+        let scrollViewSize = scrollView.bounds.size
+        let w = scrollViewSize.width / newZoomScale
+        let h = scrollViewSize.height / newZoomScale
+        let x = pointInView.x - (w / 2.0)
+        let y = pointInView.y - (h / 2.0)
+        
+        let rectZoomTo = CGRectMake(x, y, w, h)
+        scrollView.zoomToRect(rectZoomTo, animated: true)
+        
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        centerScrollViewContents()
+    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "OpenDetail"{
             if let signDetailsViewController = segue.destinationViewController.topViewController as? SignDetailsViewController{
